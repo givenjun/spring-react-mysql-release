6dd5cb89ca90058ef1fde8d8ac7a8ae10ff191be
thadus2 <152844617+thadus2@users.noreply.github.com>
2025-09-26 02:03:47 +0900

    Revert "Feature/ai chat"

--- board-back/src/main/java/com/capstone/board_back/dto/request/gemini/GeminiRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.capstone.board_back.dto.request.gemini;
-
-import lombok.Data; // Lombok을 사용하여 Getter, Setter, toString 등 자동 생성
-
-@Data
-public class GeminiRequestDto {
-    private String prompt;
-}
