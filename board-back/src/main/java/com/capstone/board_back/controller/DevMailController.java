964be4f35288b2a573b8c5eb39e659794d9249f1
givenjun <wjdwns0531@gmail.com>
2025-09-30 16:20:35 +0900

    mail test connection

--- board-back/src/main/java/com/capstone/board_back/controller/DevMailController.java
+++ board-back/src/main/java/com/capstone/board_back/controller/DevMailController.java
@@ -1,4 +1,22 @@
 package com.capstone.board_back.controller;
 
+import com.capstone.board_back.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/dev/email")
+@RequiredArgsConstructor
 public class DevMailController {
+
+    private final MailService mailService;
+
+    /** POST /api/dev/email/ping  { "to":"받는주소" } */
+    @PostMapping("/ping")
+    public void ping(@RequestBody Map<String, String> body) {
+        String to = body.get("to");
+        mailService.sendText(to, "[RoutePick] 메일 송신 테스트", "메일 발송 설정이 정상입니다 👋");
+    }
 }
